c0f3ace86dd3f06442f520be893abfd5113bc324
Rick <deathwarsoldier@gmail.com>
2020-01-29 22:19:21 -0600

    agregado menu

--- src/com/tutorial/main/Game.java
+++ src/com/tutorial/main/Game.java
@@ -28,23 +28,32 @@ public class Game extends Canvas implements Runnable{
 	private HUD hud;
 	private Handler handler;
 	private Spawn spawn;
+	private Menu menu;
+	
+	public enum STATE {
+		Menu,
+		Help,
+		Game
+	};
+	public STATE gameState = STATE.Menu;
 	/**
 	 * Constructor de la clase
 	 */
 	public Game() {
 		handler = new Handler();
+		menu = new Menu(this, handler);
 		this.addKeyListener(new KeyInput(handler));
-		
+		this.addMouseListener(menu);
 		new Window(WIDTH, HEIGHT, TITLE, this);
 		
 		hud = new HUD();
 		rnd = new Random();
+		
 		spawn = new Spawn(handler, hud);
-		
-		handler.addObject(new Player(WIDTH/2-32,HEIGHT/2-32,ID.Player,handler));
-//		for(int i = 0;i<20;i++)
-		handler.addObject(new BasicEnemy(rnd.nextInt(WIDTH-50),rnd.nextInt(HEIGHT-50),ID.BasicEnemy, handler));
-		
+		if(gameState == STATE.Game) {
+			handler.addObject(new Player(WIDTH/2-32,HEIGHT/2-32,ID.Player,handler));
+			handler.addObject(new BasicEnemy(rnd.nextInt(WIDTH-50),rnd.nextInt(HEIGHT-50),ID.BasicEnemy, handler));
+		}
 	}
 	
 	/**
@@ -122,8 +131,11 @@ public class Game extends Canvas implements Runnable{
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
 		handler.render(g);
-		hud.render(g);
-		
+		if(gameState == STATE.Game) {
+			hud.render(g);
+		}else if(gameState == STATE.Menu || gameState == STATE.Help){
+			menu.render(g);
+		}
 		g.dispose();
 		bs.show();
 	}
@@ -132,8 +144,12 @@ public class Game extends Canvas implements Runnable{
 	 */
 	private void tick() {
 		handler.tick();
-		hud.tick();
-		spawn.tick();
+		if(gameState == STATE.Game) {
+			hud.tick();
+			spawn.tick();
+		}else if (gameState == STATE.Menu || gameState == STATE.Help) {
+			menu.tick();
+		}
 	}
 
 	/**
---
+++ src/com/tutorial/main/Menu.java
@@ -0,0 +1,149 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Random;
+
+
+public class Menu extends MouseAdapter{
+	
+    private Game game;
+    private Handler handler;
+    private Random rnd = new Random();
+    
+    public Menu(Game game, Handler handler) {
+    	this.game = game;
+    	this.handler = handler;
+    }
+	
+	private static final int OPTION_PLAY_HEIGHT = 64;
+	private static final int OPTION_PLAY_WIDTH = 200;
+	private static final int OPTION_PLAY_Y = 150;
+	private static final int OPTION_PLAY_X = 210;
+	
+	private static final int OPTION_HELP_HEIGHT = 64;
+	private static final int OPTION_HELP_WIDTH = 200;
+	private static final int OPTION_HELP_Y = 250;
+	private static final int OPTION_HELP_X = 210;
+	
+	private static final int OPTION_HELP_BACK_HEIGHT = 64;
+	private static final int OPTION_HELP_BACK_WIDTH = 200;
+	private static final int OPTION_HELP_BACK_Y = 350;
+	private static final int OPTION_HELP_BACK_X = 210;
+	
+	private static final int OPTION_EXIT_HEIGHT = 64;
+	private static final int OPTION_EXIT_WIDTH = 200;
+	private static final int OPTION_EXIT_Y = 350;
+	private static final int OPTION_EXIT_X = 210;
+
+	public void mousePressed(MouseEvent e) {
+		int mouseX = e.getX();
+		int mouseY = e.getY();
+		if(game.gameState == Game.STATE.Menu) {
+			//boton jugar
+			if(mouseOver(mouseX, mouseY, OPTION_PLAY_X, OPTION_PLAY_Y, OPTION_PLAY_WIDTH, OPTION_PLAY_HEIGHT)){
+				game.gameState = Game.STATE.Game;
+				handler.addObject(new Player(Game.WIDTH/2-32,Game.HEIGHT/2-32,ID.Player,handler));
+				handler.addObject(new BasicEnemy(rnd.nextInt(Game.WIDTH-50),rnd.nextInt(Game.HEIGHT-50),ID.BasicEnemy, handler));
+			}
+			//boton ayuda
+			if(mouseOver(mouseX, mouseY, OPTION_HELP_X, OPTION_HELP_Y, OPTION_HELP_WIDTH, OPTION_HELP_HEIGHT)){
+				game.gameState = Game.STATE.Help;
+				
+			}
+			//boton salir
+			if(mouseOver(mouseX, mouseY, OPTION_EXIT_X, OPTION_EXIT_Y, OPTION_EXIT_WIDTH, OPTION_EXIT_HEIGHT)){
+				System.exit(1);
+			}
+		}else if(game.gameState == Game.STATE.Help) {
+			//boton regresar
+			if(mouseOver(mouseX, mouseY, OPTION_HELP_BACK_X, OPTION_HELP_BACK_Y, OPTION_HELP_BACK_WIDTH, OPTION_HELP_BACK_HEIGHT)){
+				game.gameState = Game.STATE.Menu;
+				
+			}
+		}
+		
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+	
+	private boolean mouseOver(int mouseX, int mouseY, int x, int y , int width, int height) {
+		if( mouseX > x && mouseX < x + width && 
+			mouseY > y && mouseY < y +height) {
+				return true;
+		}
+		return false;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render (Graphics g) {
+		if(game.gameState == Game.STATE.Menu) {
+		
+			Font font = new Font("arial",1,50);
+			Font font2 = new Font("arial",1,30);
+			
+			g.setFont(font);
+			g.setColor(Color.white);
+			g.drawString("Menu", 240, 70);
+			
+			
+			
+			g.setColor(Color.white);
+			g.drawRect(OPTION_PLAY_X, OPTION_PLAY_Y, OPTION_PLAY_WIDTH, OPTION_PLAY_HEIGHT);
+			
+			g.setFont(font2);
+			g.setColor(Color.white);
+			g.drawString("Jugar", 270, 190);
+			
+			g.setColor(Color.white);
+			g.drawRect(OPTION_HELP_X, OPTION_HELP_Y, OPTION_HELP_WIDTH, OPTION_HELP_HEIGHT);
+			
+			g.setFont(font2);
+			g.setColor(Color.white);
+			g.drawString("Ayuda", 270, 290);
+			
+			g.setColor(Color.white);
+			g.drawRect(OPTION_EXIT_X, OPTION_EXIT_Y, OPTION_EXIT_WIDTH, OPTION_EXIT_HEIGHT);
+			
+			g.setFont(font2);
+			g.setColor(Color.white);
+			g.drawString("Salir", 270, 390);
+		}else if(game.gameState == Game.STATE.Help) {
+			
+			
+			Font font = new Font("arial",1,50);
+			Font font2 = new Font("arial",1,30);
+			
+			
+			g.setFont(font);
+			g.setColor(Color.white);
+			g.drawString("Ayuda", 240, 70);
+			
+			g.setColor(Color.white);
+			g.drawRect(OPTION_HELP_BACK_X, OPTION_HELP_BACK_Y, OPTION_HELP_BACK_WIDTH, OPTION_HELP_BACK_HEIGHT);
+			
+			g.setFont(font2);
+			g.setColor(Color.white);
+			g.drawString("Usa las teclas WASD para mover al jugador", 5, 190);
+			
+			g.setFont(font2);
+			g.setColor(Color.white);
+			g.drawString(" y evitar enemigos.", 5, 230);
+			
+			
+			
+			g.setFont(font2);
+			g.setColor(Color.white);
+			g.drawString("Regresar", 240, 390);
+		}
+	}
+
+}
